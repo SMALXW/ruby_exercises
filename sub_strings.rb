7ea13b85145911308e71ae7acf4e3a97d3318851
SMALXW <samuel.a.walton@gmail.com>
2020-11-30 12:48:57 +0000

    commiting file to repo

---
+++ sub_strings.rb
@@ -0,0 +1,11 @@
+def substring(str, dictionary)
+    results = {}
+
+    str.downcase!
+
+    dictionary.each { |word| 
+        if str.include?(word)
+            results[word] = str.scan(word).count
+        end
+    results
+    }
