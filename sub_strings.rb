14e54ced2e92279819c7f337dc3d954c5b2591b4
SMALXW <samuel.a.walton@gmail.com>
2020-11-30 12:52:12 +0000

    formatting file

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,11 +1,10 @@
 def substring(str, dictionary)
     results = {}
-
     str.downcase!
-
     dictionary.each { |word| 
         if str.include?(word)
             results[word] = str.scan(word).count
         end
     results
     }
+end
